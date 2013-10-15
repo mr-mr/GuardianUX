7d2abdc842498beac2f6605f414635f32eeec268
mr-mr <mrmr.mail@gmail.com>
2013-10-15 22:01:10 +0100

    Added accordion layout, with styling to come

--- GuardianStrap/script/accordion-with-arrows.js
+++ GuardianStrap/script/accordion-with-arrows.js
@@ -5,7 +5,7 @@ $(function () {
 	containers.hide().filter(':first').show();
 
 	controls.click(function (e) {
-		containers.slideUp().filter(this.hash).slideDown();
+		//containers.slideUp().filter(this.hash).slideDown();
 
 		controls.removeClass('is-current').children('i').removeClass('icon-angle-up').addClass('icon-angle-down');
 		$(this).addClass('is-current').children('i').removeClass('icon-angle-down').addClass('icon-angle-up');
