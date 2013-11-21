92604f8576fcd1fa78ac0be4d40c501beff8b7b8
Chris Clarke <chris.j.clarke@guardian.co.uk>
2013-11-21 16:39:30 +0000

    Commercial components
    Added CC to a container in the footer.
    Ear shows at certain point on page.
    Example page of breakpoints is styled correctly

--- GuardianNGW/script/sticky-position.js
+++ GuardianNGW/script/sticky-position.js
@@ -1,17 +1,19 @@
-var sticky = document.querySelector('.c-nav');
-var origOffsetY = 100;
+var sticky = document.querySelector('.ears-nav');
+var origOffsetY = 1400;
+var endY = 2800;
 
 function onScroll() {
-  window.scrollY >= origOffsetY ? sticky.classList.add('fixed') :
-                                  sticky.classList.remove('fixed');
+	currentPosition = window.scrollY;
+  currentPosition >= origOffsetY && currentPosition <= endY ? sticky.classList.add('ears-nav-open') :
+                                  sticky.classList.remove('ears-nav-open');
 }
 
-/*var stickyAD = document.querySelector('aside.right');
-var origOffsetY = 390;
+// var stickyAD = document.querySelector('aside.right');
+// var origOffsetY = 390;
 
-function onScroll() {
-  window.scrollY >= origOffsetY ? stickyAD.classList.add('fixed.advert') :
-                                  stickyAD.classList.remove('fixed.advert');
-}
+// function onScroll() {
+//   window.scrollY >= origOffsetY ? stickyAD.classList.add('fixed.advert') :
+//                                   stickyAD.classList.remove('fixed.advert');
+// }
 
-document.addEventListener('scroll', onScroll);*/
+document.addEventListener('scroll', onScroll);
