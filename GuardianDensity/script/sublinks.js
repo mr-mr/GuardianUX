f30808a685f3a082d391fac9379bf4da5f2cc1ee
Chris Clarke <chris.j.clarke@guardian.co.uk>
2014-09-09 10:18:39 +0100

    updated card sizes

--- GuardianDensity/script/sublinks.js
+++ GuardianDensity/script/sublinks.js
@@ -1,7 +1,31 @@
 //$(".rasiak-expand-circle").hide();
 //alert("I'm loading!");
 
-$(".rasiak-expand-circle").click(function(e){
-	e.preventDefault();
-	$(".rasiak-sublinks").animate({ "margin-top": "-100px"}), 3000, ("linear");
+$(".rasiak-expand-circle").click( function(event){
+	event.preventDefault();
+	if ($(this).hasClass("isDown") ) {
+		$(".rasiak-sublinks-scroll").animate({"marginTop":"-5px"}, 400);			
+		$(this).removeClass("isDown");
+		$(".rasiak-expand-arrow").toggleClass("sublinks-arrow-rotate");
+	} else {
+		$(".rasiak-sublinks-scroll").animate({"marginTop":"-105px"}, 400);	
+		$(this).addClass("isDown");
+		$(".rasiak-expand-arrow").toggleClass("sublinks-arrow-rotate");
+	}
+	return false;
+});
+
+
+$(".rasiak-meta").click( function(event){
+	event.preventDefault();
+	if ($(this).hasClass("isDown") ) {
+		$(".rasiak-sublinks-scroll-2").animate({"marginTop":"-5px"}, 400);			
+		$(this).removeClass("isDown");
+		$("icon-caret-down").switchClass("icon-caret-down", "icon-caret-up");
+	} else {
+		$(".rasiak-sublinks-scroll-2").animate({"marginTop":"-105px"}, 400);	
+		$(this).addClass("isDown");
+		$("icon-caret-up").switchClass("icon-caret-up", "icon-caret-down");
+	}
+	return false;
 });
